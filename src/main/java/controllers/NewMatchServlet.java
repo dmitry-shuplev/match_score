755b19d9b63929c3a716c6f8f0d4c5af28755278
j_dim <dmitrijsuplev@gmail.com>
2025-08-22 16:22:09 +0300

    feat:change_match_servlet

--- src/main/java/controllers/NewMatchServlet.java
+++ src/main/java/controllers/NewMatchServlet.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
 @WebServlet("/new-match")
-public class NewMatchServlet extends HttpServlet{
+public class NewMatchServlet extends HttpServlet {
 }
