4e1332ebd94a677aedcc63eb12687ce3ad469ea3
j_dim <dmitrijsuplev@gmail.com>
2025-08-26 15:32:39 +0300

    feat:show_new_match_jsp

--- src/main/java/controllers/NewMatchServlet.java
+++ src/main/java/controllers/NewMatchServlet.java
@@ -1,8 +1,16 @@
 package controllers;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet("/new-match")
-public class NewMatchServlet extends HttpServlet {
+public class NewMatchServlet extends HttpServlet  {
+   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       request.getRequestDispatcher("/new-match.jsp").forward(request, response);
+   }
 }
