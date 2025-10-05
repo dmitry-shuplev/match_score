79059e3f5cab14d27bc0f8da774ca23e923d2f01
j_dim <dmitrijsuplev@gmail.com>
2025-10-05 08:13:16 +0300

    feat:pagination_in_matches

--- src/main/java/controllers/MatchesServlet.java
+++ src/main/java/controllers/MatchesServlet.java
@@ -17,7 +17,8 @@ public class MatchesServlet extends HttpServlet {
     private final int NotesInPalge = 15;
     MatchesHadler matchesHadler;
     MatchesDao matchesDao;
-
+    int currentPage;
+    int totalPages;
 
     @Override
     public void init() throws ServletException {
@@ -29,7 +30,11 @@ public class MatchesServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         List<MatchWebDto> matchesWebDto = matchesHadler.convertToMatchWebDto(matchesDao.getAllMatches());
-
+        currentPage = 1;
+        int notes = matchesWebDto.size();
+        totalPages = (int) Math.ceil((double) notes/NotesInPalge);
+        System.out.println(notes);
+        System.out.println(totalPages);
         request.setAttribute("matchWebDto", matchesWebDto);
         request.getRequestDispatcher("/matches.jsp").forward(request, response);
     }
