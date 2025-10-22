8d176076fb3f5a1a0053337e0a6998c7c8384c36
j_dim <dmitrijsuplev@gmail.com>
2025-10-22 09:08:20 +0300

    test:add_create_player_test

--- src/main/java/service/PlayerDao.java
+++ src/main/java/service/PlayerDao.java
@@ -7,6 +7,9 @@ import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import utils.HibernateUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerDao {
     SessionFactory sessionFactory;
 
@@ -14,9 +17,16 @@ public class PlayerDao {
         sessionFactory = sf;
     }
 
+    public List<Player> getAll(){
+    List<Player> allPlayers = new ArrayList<>();
+
+
+    return allPlayers;
+    }
+
     public Player getById(int id) {
         Player player = null;
-        try(Session session = sessionFactory.openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             player = session.find(Player.class, id);
         } catch (Exception e) {
             e.printStackTrace();
@@ -38,37 +48,57 @@ public class PlayerDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-        e.printStackTrace();
+            e.printStackTrace();
         }
-            return player;
+        return player;
+    }
+
+    public void deleteByName(String name) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            String hql = "DELETE FROM Player p WHERE p.name = :name";
+            Query query = session.createQuery(hql);
+            query.setParameter("name", name);
+            query.executeUpdate();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
         }
 
-        public void crateNewPlayer(String name){
+    }
+
+    public void crateNewPlayer(String name) {
         Transaction transaction = null;
         Player player = new Player();
-        try(Session session = sessionFactory.openSession()){
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             player.setName(name);
             session.persist(player);
         }
-        };
-
-
-        public String nameFormated (String inputName){
-            String cleanedInput = inputName.replaceAll("[^a-zA-zа-яА-ЯёЁ'\s]", "");
-            String[] names = cleanedInput.split("\s+");
-            String formatedString = "";
-            for (String name : names) {
-                if (!name.isEmpty()) {
-                    formatedString += name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase()
-                            + " ";
-                }
-            }
-            return formatedString.trim();
-        }
-
-        public boolean isCurrentPlayerExist(String inputName){
-        return getByName(inputName)!=null;
-        }
-
     }
+
+    ;
+
+
+    public String nameFormated(String inputName) {
+        String cleanedInput = inputName.replaceAll("[^a-zA-zа-яА-ЯёЁ'\s]", "");
+        String[] names = cleanedInput.split("\s+");
+        String formatedString = "";
+        for (String name : names) {
+            if (!name.isEmpty()) {
+                formatedString += name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase()
+                        + " ";
+            }
+        }
+        return formatedString.trim();
+    }
+
+    public boolean isCurrentPlayerExist(String inputName) {
+        return getByName(inputName) != null;
+    }
+
+}
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,8 +1,59 @@
-import org.junit.jupiter.api.Test;
+import models.Player;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.*;
+import service.PlayerDao;
+import utils.HibernateUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PlayerTest {
-    @Test
-    public void FormatPlayerNameRight(){
+    static PlayerDao pd;
+    String expectedPlayerName;
+    String getingPlayerName;
 
+    @BeforeAll
+    static void createPlayerDao() {
+        SessionFactory sf = HibernateUtils.getSessionFactory();
+        pd = new PlayerDao(sf);
+    }
+
+    @BeforeEach
+    void createInputParametr() {
+        expectedPlayerName = "Гурген Гургенович";
+        getingPlayerName = "гурген 123 гургенович     ";
+    }
+
+    @Test
+    public void FormatPlayerNameRight() {
+        String formatedPlayerName = pd.nameFormated(getingPlayerName);
+        assertEquals(expectedPlayerName, formatedPlayerName);
+    }
+
+    @Test
+    public void writenToDb() {
+        Player player = new Player();
+        pd.crateNewPlayer(expectedPlayerName);
+        player = pd.getByName(expectedPlayerName);
+        assertTrue(player.getName().equals(expectedPlayerName));
+        pd.deleteByName(expectedPlayerName);
+    }
+
+    @Test
+    public void priventDuplicatePlayer(){
+        pd.crateNewPlayer(expectedPlayerName);
+        pd.crateNewPlayer(expectedPlayerName);
+
+    }
+
+    @AfterEach
+    void destroyInputParament() {
+        expectedPlayerName = null;
+        getingPlayerName = null;
+    }
+
+    @AfterAll
+    static void destryouCoonectionDB() {
+        HibernateUtils.shutDown();
     }
 }
